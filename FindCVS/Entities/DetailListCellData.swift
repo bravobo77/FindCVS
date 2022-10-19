88c676eef7f18bcd735360c7a062ed595158fe25
bravobo77 <a95067300@gmail.com>
2022-10-19 15:50:11 +0900

    add entities

---
+++ FindCVS/Entities/DetailListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  DetailListCellData.swift
+//  FindCVS
+//
+//  Created by 金　甫硬 on 2022/10/19.
+//
+
+import Foundation
+
+struct DetailListCellData {
+    let placeName: String
+    let address: String
+    let distance: String
+    let point: MTMapPoint
+}
---
+++ FindCVS/Entities/LocationData.swift
@@ -0,0 +1,12 @@
+//
+//  LocationData.swift
+//  FindCVS
+//
+//  Created by 金　甫硬 on 2022/10/19.
+//
+
+import Foundation
+
+struct LocationData: Decodable {
+    let documents: [KLDocument]
+}
